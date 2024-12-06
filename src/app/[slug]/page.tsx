58208683f8abf6bfda0d5148402782610fa68d35
William Gardner <william@madebycounter.com>
2024-12-05 16:40:17 -0800

    Added 404

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 import SitePage from "@/site/SitePage";
 
 import { usePage } from "@/lib/queries";
@@ -10,6 +12,10 @@ export default async function Page({
     };
 }) {
     const pageData = await usePage(params.slug);
+
+    if (!pageData) {
+        return notFound();
+    }
 
     return <SitePage pageData={pageData} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from "next/navigation";
+
 import SitePage from "@/site/SitePage";
 
 import { useHomePage } from "@/lib/queries";
 
 export default async function HomePage() {
     const pageData = await useHomePage();
+
+    if (!pageData) {
+        return notFound();
+    }
 
     return <SitePage pageData={pageData} />;
 }
